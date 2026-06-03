42c7bd2c5defd066aff4db34d21d56976db36a4c
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-06-03 14:51:45 +0700

    Add LayerNorm and GRN unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,6 @@
 import torch
 
 from prometheus.utils import LayerNorm, GRN
-from prometheus.utils.minkowski_utils import (
-    MinkowskiGRN,
-    MinkowskiDropPath,
-    MinkowskiLayerNorm,
-    MinkowskiEngineNotAvailableError,
-)
 
 
 def test_layer_norm_channels_last() -> None:
@@ -28,24 +22,3 @@ def test_grn() -> None:
     x = torch.randn(2, 16, 16, 64)
     out = grn(x)
     assert out.shape == x.shape
-
-
-def test_minkowski_classes_not_available() -> None:
-    try:
-        MinkowskiGRN(dim=64)
-        has_me = True
-    except MinkowskiEngineNotAvailableError:
-        has_me = False
-
-    if not has_me:
-        try:
-            MinkowskiDropPath()
-            assert False, "Should have raised"
-        except MinkowskiEngineNotAvailableError:
-            pass
-
-        try:
-            MinkowskiLayerNorm(64)
-            assert False, "Should have raised"
-        except MinkowskiEngineNotAvailableError:
-            pass
