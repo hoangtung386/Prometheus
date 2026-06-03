c9c61364eca5a6d8ecc3be75f78bd37a37dd5438
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-06-03 14:51:08 +0700

    Export losses and models at package level

--- src/prometheus/__init__.py
+++ src/prometheus/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .losses import (
     BCEWithLogitsLoss,
@@ -10,12 +10,10 @@ from .losses import (
 )
 from .models.unet_dual import DualUNet
 from .models.unet_tissue import UNet as UNetTissue
-from .models.unet_nuclei import UNet as UNetNuclei
 
 __all__ = [
     "DualUNet",
     "UNetTissue",
-    "UNetNuclei",
     "BCEWithLogitsLoss",
     "DiceLoss",
     "FocalLoss",
