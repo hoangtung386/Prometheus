aad00941071bb6c1b7e2b6c8e11629b9ea40ecdd
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-06-03 14:51:12 +0700

    Clean up models __init__ exports

--- src/prometheus/models/__init__.py
+++ src/prometheus/models/__init__.py
@@ -1,9 +1,7 @@
 from .unet_dual import DualUNet
 from .unet_tissue import UNet as UNetTissue
-from .unet_nuclei import UNet as UNetNuclei
 
 __all__ = [
     "DualUNet",
     "UNetTissue",
-    "UNetNuclei",
 ]
