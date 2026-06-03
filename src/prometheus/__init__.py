3a06db97151ed3ccbfa9bad75b2f40e68d896df4
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-06-03 13:45:13 +0700

    Export loss functions from top-level package

--- src/prometheus/__init__.py
+++ src/prometheus/__init__.py
@@ -1,5 +1,13 @@
 __version__ = "0.1.0"
 
+from .losses import (
+    BCEWithLogitsLoss,
+    CombinedLoss,
+    DiceLoss,
+    FocalLoss,
+    MultiClassDiceLoss,
+    TverskyLoss,
+)
 from .models.unet_dual import DualUNet
 from .models.unet_tissue import UNet as UNetTissue
 from .models.unet_nuclei import UNet as UNetNuclei
@@ -8,4 +16,10 @@ __all__ = [
     "DualUNet",
     "UNetTissue",
     "UNetNuclei",
+    "BCEWithLogitsLoss",
+    "DiceLoss",
+    "FocalLoss",
+    "CombinedLoss",
+    "MultiClassDiceLoss",
+    "TverskyLoss",
 ]
