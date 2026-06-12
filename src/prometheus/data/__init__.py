a05436200496be54fc2efda63099f31edb30de9e
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-06-12 10:39:12 +0700

    Export test_transform in data __init__

--- src/prometheus/data/__init__.py
+++ src/prometheus/data/__init__.py
@@ -1,9 +1,10 @@
 from .puma_dataset import PUMADataset, create_puma_dataloaders
-from .transforms import train_transform, val_transform, collate_puma
+from .transforms import test_transform, train_transform, val_transform, collate_puma
 
 __all__ = [
     "PUMADataset",
     "create_puma_dataloaders",
+    "test_transform",
     "train_transform",
     "val_transform",
     "collate_puma",
