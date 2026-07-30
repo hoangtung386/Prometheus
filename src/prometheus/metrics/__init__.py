7fc970d4cdb48a37ec171b72e2ab9524bbec0941
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-07-30 13:52:40 +0700

    refactor: remove metrics/evaluator.py; add segmentation.py with official_micro_dice

--- src/prometheus/metrics/__init__.py
+++ src/prometheus/metrics/__init__.py
@@ -1,6 +1,8 @@
-from .evaluator import SegmentationEvaluator
+"""Tissue segmentation and nuclei detection metrics matching the official evaluator."""
+
 from .matching import Match, MatchResult, match_detections
 from .nuclei_detection import nuclei_detection_metrics
+from .segmentation import SegmentationEvaluator, official_micro_dice
 
 __all__ = [
     "Match",
@@ -8,4 +10,5 @@ __all__ = [
     "SegmentationEvaluator",
     "match_detections",
     "nuclei_detection_metrics",
+    "official_micro_dice",
 ]
