bf3b4dc7cb8cfe99b94049bc5b436df39756710a
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-07-30 13:47:08 +0700

    refactor: update data __init__ with new exports (collate, spatial)

--- src/prometheus/data/__init__.py
+++ src/prometheus/data/__init__.py
@@ -1,7 +1,14 @@
+"""PUMA parsing, instance-aware datasets, transforms, targets and collators."""
+
+from .collate import collate_multitask
 from .puma import PumaMultitaskDataset, create_multitask_dataloaders, create_multitask_kfold_dataloaders
+from .spatial import letterbox_image, restore_mask
 
 __all__ = [
     "PumaMultitaskDataset",
+    "collate_multitask",
     "create_multitask_dataloaders",
     "create_multitask_kfold_dataloaders",
+    "letterbox_image",
+    "restore_mask",
 ]
