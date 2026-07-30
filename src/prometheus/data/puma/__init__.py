163843e0e537bdfe5c98d34b8d474ecc8033b7fc
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-07-30 13:47:21 +0700

    refactor: update puma/__init__.py exports; remove class taxonomy (moved to domain)

--- src/prometheus/data/puma/__init__.py
+++ src/prometheus/data/puma/__init__.py
@@ -1,26 +1,32 @@
-"""PUMA dataset discovery, parsing, rasterization and datasets."""
+"""PUMA dataset discovery, parsing, rasterization, splits and datasets.
 
-from .cellvit_export import CELLVIT_PUMA_CLASS_ORDER, export_cellvit_dataset
-from .classes import (
-    NUCLEI_CLASS_TO_IDX,
-    NUCLEI_CLASSES,
-    TISSUE_CLASS_TO_IDX,
-    TISSUE_CLASSES,
-)
-from .loaders import (
-    create_multitask_dataloaders,
-    create_multitask_kfold_dataloaders,
-)
-from .multitask_dataset import PumaMultitaskDataset
+The class taxonomy lives in :mod:`prometheus.domain.labels`, not here: it is a domain
+contract shared by the data, metrics and io layers.
+"""
+
+from .audit import audit_puma_dataset, audit_resolution, audit_tissue_rasterization
+from .cellvit_export import export_cellvit_dataset
+from .dataset import PumaMultitaskDataset, read_native_image
+from .discovery import discover_puma_samples
+from .geojson import parse_nuclei_geojson, parse_tissue_geojson
+from .loaders import create_multitask_dataloaders, create_multitask_kfold_dataloaders
+from .rasterize import TISSUE_PAINT_PRIORITY, rasterize_tissue_regions
+from .splits import load_or_create_kfold, load_or_create_split
 
 __all__ = [
-    "CELLVIT_PUMA_CLASS_ORDER",
-    "NUCLEI_CLASSES",
-    "NUCLEI_CLASS_TO_IDX",
+    "TISSUE_PAINT_PRIORITY",
     "PumaMultitaskDataset",
-    "TISSUE_CLASSES",
-    "TISSUE_CLASS_TO_IDX",
+    "audit_puma_dataset",
+    "audit_resolution",
+    "audit_tissue_rasterization",
     "create_multitask_dataloaders",
     "create_multitask_kfold_dataloaders",
+    "discover_puma_samples",
     "export_cellvit_dataset",
+    "load_or_create_kfold",
+    "load_or_create_split",
+    "parse_nuclei_geojson",
+    "parse_tissue_geojson",
+    "rasterize_tissue_regions",
+    "read_native_image",
 ]
