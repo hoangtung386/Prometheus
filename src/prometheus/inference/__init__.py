890735ef84d4ffdff07703ad96b1fb83fa0ecbbb
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-07-30 13:51:01 +0700

    refactor: update inference __init__ with TTA exports

--- src/prometheus/inference/__init__.py
+++ src/prometheus/inference/__init__.py
@@ -1,10 +1,14 @@
-"""Center-based instance inference API."""
+"""Center-based instance inference and test-time augmentation."""
 
 from .nuclei_decoder import decode_nuclei
 from .predictor import MultitaskPrediction, PrometheusPredictor
+from .tta import DIHEDRAL_VIEWS, FLIP_VIEWS, tta_forward
 
 __all__ = [
+    "DIHEDRAL_VIEWS",
+    "FLIP_VIEWS",
     "MultitaskPrediction",
     "PrometheusPredictor",
     "decode_nuclei",
+    "tta_forward",
 ]
