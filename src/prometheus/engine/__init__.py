37890398cd7c387fbb0f837b0a8d1618d0fb7e29
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-07-30 13:50:12 +0700

    refactor: update engine __init__ with new exports (ema, schedule, validation)

--- src/prometheus/engine/__init__.py
+++ src/prometheus/engine/__init__.py
@@ -1,3 +1,5 @@
+"""Training, validation and versioned checkpointing."""
+
 from .checkpointing import (
     CHECKPOINT_SCHEMA_VERSION,
     assert_checkpoint_compatible,
@@ -5,16 +7,22 @@ from .checkpointing import (
     save_engine_checkpoint,
     select_inference_state,
 )
-from .evaluator import EvaluationResult, evaluate_multitask
-from .trainer import PrometheusTrainer
+from .ema import WeightEma
+from .schedule import build_optimizer, warmup_cosine_lambda
+from .trainer import PrometheusTrainer, seed_everything
+from .validation import EvaluationResult, evaluate_multitask
 
 __all__ = [
     "CHECKPOINT_SCHEMA_VERSION",
     "EvaluationResult",
     "PrometheusTrainer",
+    "WeightEma",
     "assert_checkpoint_compatible",
+    "build_optimizer",
     "evaluate_multitask",
     "load_engine_checkpoint",
     "save_engine_checkpoint",
+    "seed_everything",
     "select_inference_state",
+    "warmup_cosine_lambda",
 ]
